9f0ffc9867c691fb07fe03030e32d9f610bc2ced
ams42 <ams4251234@naver.com>
2025-09-26 13:57:27 +0900

    Basic screen layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="screen">
+      <div className="phone">
+        <h1 className="logo">INTEL FIT</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
