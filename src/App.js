fb8c32f7892de17f6b19b19af0d0a77dc71ddaf6
donggyu412 <guya4226@gmail.com>
2025-09-29 17:03:44 +0900

    홈화면 pull

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 import "./App.css";
+import {Routes, Route} from "react-router-dom";  
+import Report from "./pages/Report";
+import Community from "./pages/community";
+import Recommend from "./pages/Recommend";
+import Home from "./pages/Home";  
+
+// React Router를 이용한 페이지 라우팅 설정
+// 각 경로에 따라 다른 컴포넌트를 렌더링
+// 1. "/" : Home페이지
+// 2. "/community" : 커뮤니티 페이지
+// 3. "/report" : AI 리포트 페이지
+// 4. "/recommend" : AI 추천 페이지
 
 function App() {
   return (
