7374786988fa0317386dc55c30479811683d8e45
donggyu <guya4226@gmail.com>
2025-10-31 15:32:49 +0900

    헤더위치변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import styles from "./Header.module.css";
 
 export default function Header({ title }) {
   return (
-    <header className={styles['app-header']}>
-      <h1 className={styles['title']}>{title}</h1>
-      <Bell />
+    <header className={styles["app-header"]}>
+      <h1 className={styles["title"]}>{title}</h1>
+      <div className={styles["bell-container"]}>
+        <Bell />
+      </div>
     </header>
   );
 }
